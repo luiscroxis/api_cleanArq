06e0553412464e3c9481910fda56b48722226b1d
Luis Carlos Silva <luiscroxis@gmail.com>
2023-11-07 18:43:41 -0300

    feat: update methods

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -16,14 +16,30 @@ export class UserEntity extends Entity<UserProps> {
     this.props.createdAt = this.props.createdAt ?? new Date()
   }
 
+  update(value: string): void {
+    this.name = value
+  }
+
+  updatePassword(value: string): void {
+    this.password = value
+  }
+
   get name() {
     return this.props.name
+  }
+
+  private set name(value: string) {
+    this.props.name = value
   }
   get email() {
     return this.props.email
   }
   get password() {
     return this.props.password
+  }
+
+  private set password(value: string) {
+    this.props.password = value
   }
 
   get createdAt() {
